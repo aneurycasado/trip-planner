08756b02d89d2d484cbf78197e1a8b6429c312cf
= <=>
2015-08-19 13:05:25 -0400

    Models made_bugs

---
+++ models/index.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/wikistack');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+var Place, Hotel, Restaurant, Activity;
+
+var placeSchema = new Schema({
+  address: { type: String, required: true },
+	city: {required: true},
+	state: {type: String, required: true},
+	location: {type: [], required: true},
+	phone: {type: String, required: true}
+	
+});
+
+var hotelSchema = new Schema({
+	
+	name: {type: String, required: true},
+	place: {type: Schema.ObjectId, ref: 'Place'},
+	num-stars: {type: Number, max:5, min:1},
+	amenities: {type: [String], get: function(tags){ return tags.join(', ') }},
+});
+
+var activitySchema = new Schema({
+	
+	name: {type: String, required: true},
+	place: {type: Schema.ObjectId, ref: 'Place'},
+	age-range: {type: String}
+});
+
+
+var restaurantSchema = new Schema({
+	
+	name: {type: String, required: true},
+	place: {type: Schema.ObjectId, ref: 'Place'},
+	price: {type: Number, max:5, min:1},
+	cuisines: {type: [String], get: function(tags){ return tags.join(', ') }},
+});
+
+Place = mongoose.model('Place', placeSchema);
+Hotel = mongoose.model('Hotel', hotelSchema);
+Activity = mongoose.model('Activity', activitySchema);
+Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+
+module.exports = {
+	Place: Place,
+	Hotel : Hotel,
+	Activity: Activity,
+	Restaurant: Restaurant,
+	
+}
